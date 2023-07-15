61303a1277c425a2909e72d2368c7457b8b7a622
khaled <empeshy20036969@example.com>
2023-07-15 10:08:45 +0300

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, tens_i, ones_i, tens_j, ones_j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			tens_i = i / 10;
+			ones_i = i % 10;
+			tens_j = j / 10;
+			ones_j = j % 10;
+
+			putchar(tens_i + '0');
+			putchar(ones_i + '0');
+			putchar(' ');
+			putchar(tens_j + '0');
+			putchar(ones_j + '0');
+
+			if (i != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
+
