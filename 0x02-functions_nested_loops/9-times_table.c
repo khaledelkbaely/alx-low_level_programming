efebb691f6ca8b81bf629548915147c42b320eee
khaled <empeshy20036969@example.com>
2023-07-18 13:19:56 +0300

    edit on task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,21 @@ void times_table(void)
 		{
 			int num = i * j;
 
+			if (num > 9)
+			{
+				int first = num / 10;
+				int second = num % 10;
+
+				_putchar(first + '0');
+				_putchar(second + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+
 			_putchar(num + '0');
 			if (j != 9)
 			{
