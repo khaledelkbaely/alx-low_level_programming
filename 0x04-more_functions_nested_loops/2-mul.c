4c73cde7bc082febf0105489d2598865a535b24f
khaled <empeshy20036969@example.com>
2023-07-21 17:31:38 +0300

    3rd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ *
+ * @a: first int to multiply
+ *
+ * @b: second int to multiply
+ *
+ * Return: a multieplied by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
