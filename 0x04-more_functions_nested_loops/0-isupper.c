43c52fd45c2b04e27e7f07cac4a6f969e23b7e74
khaled <empeshy20036969@example.com>
2023-07-21 16:04:49 +0300

    1st task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isupper - main entry
+ *
+ * @c - char to check
+ *
+ * Return: 1 if upper otherwise 0
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 26)
+	{
+		if (c == i 'a')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
