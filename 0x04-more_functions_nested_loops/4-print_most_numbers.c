1cb4903be08ed13648e58add1fb11bbc58ae1d63
khaled <empeshy20036969@example.com>
2023-07-21 17:45:33 +0300

    5th task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Return: this functions print from 0 to 9 doesnt return anything
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
