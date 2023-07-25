b5898e8236c51d144c91c9b765bbea4b3c3d853f
khaled <carnageindia2003@gmail.com>
2023-07-25 13:11:09 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * @n: pointer to the int wanted to change
+ *
+ * Return: does not return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
