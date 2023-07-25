d18e1d74f0d2dce8e3dadf2c47a7e8f905801845
khaled <carnageindia2003@gmail.com>
2023-07-25 14:04:44 -0700

    3rd task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * @s: string to measure
+ *
+ * Return: return the length of the string s
+ */
+
+void _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
