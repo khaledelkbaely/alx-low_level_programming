78eeb84f575ab58248114c44e14014dabe219712
khaled <empeshy20036969@example.com>
2023-07-27 19:13:44 +0300

    10th task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - entry point
+ *
+ * @dest: buffer to copy to
+ * @src: string to copy to buffer
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, j;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (j = 0; j <= len; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
