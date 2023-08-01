4cd8d77c454d0ca95ea1fc5480fd9d4879d5a047
khaled <empeshy20036969@example.com>
2023-08-01 19:06:21 +0300

    3rd taks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - entry point
+ *
+ * @s: string to search in
+ * @c: char to search for in s
+ *
+ * Return: pointer to the first occurance of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
