83e2f3929d75cc86b7aece744014142ade5915f3
khaled <empeshy20036969@example.com>
2023-08-01 18:28:17 +0300

    1st task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset -entry point
+ *
+ * @s: string to edit
+ * @b: byte to replace the n byte by it
+ * @n: number of bytes to replce at the start of the string
+ *
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < size)
+	{
+		s[i] = b;
+
+		i++;
+	}
+	return (s);
+}
