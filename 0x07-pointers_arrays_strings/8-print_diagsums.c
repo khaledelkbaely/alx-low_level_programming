c88dae96d3c637f888a9a8e070cc73bc84b47df4
khaled <empeshy20036969@example.com>
2023-08-02 18:34:13 +0300

    9th task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagsums - entry point
+ *
+ * @a: array to find its diagonal sum
+ * @size: number of items in the array
+ *
+ * Return: does not return
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = sum2 = 0;
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum1 += *(a + ((i * size) + j));
+			if (i + j == size - 1)
+				sum2 += *(a + ((i * size) + j));
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
