c711ceec19bfe9e34c509d8246ce1789d710ba76
khaled <empeshy20036969@example.com>
2023-08-02 19:10:58 +0300

    11th task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - entry point
+ *
+ * @s: pointer to pointer to char
+ * @to: string to change to
+ *
+ * Return: does not return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
