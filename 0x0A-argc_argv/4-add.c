37d27768423add99555c7e5f4e1ca027e5b05fd9
khaled <empeshy20036969@example.com>
2023-08-04 22:49:39 +0300

    0x0A #5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of args passed
+ * @argv: array of args passed
+ *
+ * Return: 0 if success else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	result = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] > '9' || *argv[i] < '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
