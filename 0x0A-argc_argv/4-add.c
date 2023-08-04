8fec961873b2bf96a436104030b153c7b80a9cf0
khaled <empeshy20036969@example.com>
2023-08-04 22:55:16 +0300

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,10 +23,14 @@ int main(int argc, char *argv[])
 	result = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] > '9' || *argv[i] < '0')
+		int j;
+		for (j = 0; *(argv[i] + j); j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (*(argv[i] + j) > '9' || *(argv[i] + j) < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		result += atoi(argv[i]);
 	}
