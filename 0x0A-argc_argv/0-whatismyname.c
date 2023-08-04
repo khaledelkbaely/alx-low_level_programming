0f59c752a6f213272908d3b2826e32e2814c2c29
khaled <empeshy20036969@example.com>
2023-08-04 20:38:22 +0300

    0x0A #1

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: array of the arguments passed
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (*(argv[0] + i) != '\0')
+	{
+		_putchar(*(argv[0] + i));
+		i++;
+	}
+	_putchar('\n');
+}
