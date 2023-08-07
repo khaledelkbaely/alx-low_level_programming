829dc23a5f8f38f0c6cb37fc8a67608892cd1933
khaled <empeshy20036969@example.com>
2023-08-08 02:38:16 +0300

    edit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,27 +16,21 @@ char *str_concat(char *s1, char *s2)
 	char *catedstring;
 	int i, j;
 
-	if (s1 == NULL && s2)
-		return (s2);
-	if (s2 == NULL && s1)
-		return (s1);
-	if (s2 == NULL && s2 == NULL)
-		return ("");
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
 		len2++;
-
 	catedstring = malloc((len1 + len2 + 1) * sizeof(char));
 	if (catedstring == NULL)
 		return (NULL);
-
 	for (i = 0; i < len1; i++)
 		catedstring[i] = s1[i];
 	for (j = 0; j < len2; j++)
 		catedstring[i + j] = s2[j];
 	catedstring[i + j] = '\0';
-
 	return (catedstring);
 }
