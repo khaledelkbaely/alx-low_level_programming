f9a049e7f4d555e98652fc7d0a0da1b7fcd2c33f
khaled <empeshy20036969@example.com>
2023-08-08 04:18:10 +0300

    edit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * get_total_length - get the len
+ *
+ * @ac: number of arguments
+ * @av: array of string of the arguments
+ *
+ * Return: string
+ */
+
+int get_total_length(int ac, char **av)
+{
+	int total_length = 0;
+	char *arg;
+	int j;
+
+	for (int i = 0; i < ac; i++)
+	{
+		arg = av[i];
+		j = 0;
+		while (arg[j] != '\0')
+		{
+			total_length++;
+			j++;
+		}
+		total_length++;
+	}
+	return (total_length);
+}
 
 /**
  * argstostr - concatenates all arguments of a program
@@ -13,30 +41,34 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *new_string;
-	int i, arg, len, length;
+	int total_length;
+	char *result;
+	int current_index;
+	int i, j;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	len = 0;
-	i = 0;
-	while (i < ac)
-	{
-		len += strlen(av[i]) + 1;
-	}
-	new_string = malloc((len + 1) * sizeof(char));
-	if (new_string == NULL)
+	total_length = get_total_length(ac, av);
+
+	result = (char *)malloc((total_length + 1) * sizeof(char));
+	if (result == NULL)
 		return (NULL);
 
-	arg = 0;
+	current_index = 0;
 	for (i = 0; i < ac; i++)
 	{
-		length = strlen(av[i]);
-		strncpy(new_string + arg, av[i], length);
-		arg += length;
-		new_string[arg++] = '\n';
+		arg = av[i];
+		j = 0;
+		while (arg[j] != '\0')
+		{
+			result[current_index++] = arg[j++];
+		}
+		result[current_index++] = '\n';
 	}
-	new_string[len] = '\0';
-	return (new_string);
+
+	result[total_length] = '\0';
+
+	return (result);
+
 }
