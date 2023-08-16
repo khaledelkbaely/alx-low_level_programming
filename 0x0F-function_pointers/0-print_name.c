a222654da37b0b8770ad58cd86afc90ab2a65725
khaled <empeshy20036969@example.com>
2023-08-16 09:27:47 +0300

    func pointers #1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name to print
+ * @f: pointer to func to print
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
