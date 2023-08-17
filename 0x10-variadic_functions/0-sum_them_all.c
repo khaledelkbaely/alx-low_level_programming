8ab7177c242267ee1156a72deca5d0aca06cac51
khaled <empeshy20036969@example.com>
2023-08-17 11:54:58 +0300

    var func #1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all parameters
+ *
+ * @n: number of args
+ *
+ * Return: the sum of all args passed in
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	return (sum);
+}
