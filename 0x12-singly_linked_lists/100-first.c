cf4477a6ef22b1003ed8cbdc038f1b175fee8bac
youssefelsayed47 <youssefjo4433@gmail.com>
2023-08-24 21:51:42 +0300

    single linked list #6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,7 @@ void before_main(void) __attribute__((constructor));
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	char *s = "I bore my house upon my back!\n";
+
+	printf("You're beat! and yet, you must allow,\n%s", s);
 }
