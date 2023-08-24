7409bb4389c9d65d8dcaaa3e4eeab5a569789f55
youssefelsayed47 <youssefjo4433@gmail.com>
2023-08-24 18:58:13 +0300

    single linked list #3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - add node to the begining of the list
+ *
+ * @head: head of the list
+ * @str: string to copy in the node
+ *
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len;
+	list_t *temp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (new);
+	if (str)
+	{
+		new->str = strdup(str);
+		for (len = 0; str[len]; len++)
+			;
+		new->len = len;
+		new->next = NULL;
+	}
+
+	if (!head)
+		*head = new;
+	else
+	{
+		temp = *head;
+		*head = new;
+		new->next = temp;
+	}
+
+	return (*head);
+}
