bed93669beadbfc77d442ee6541ca405d3eac458
youssefelsayed47 <youssefjo4433@gmail.com>
2023-08-24 20:37:45 +0300

    single linked list #4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
  * Return: address of the new element
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp;
