663e0774146b421f9d121b5b6b0096245b9749e1
youssefelsayed47 <youssefjo4433@gmail.com>
2023-08-29 14:53:02 +0300

    single linked list #2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,17 +12,17 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
-    
-    if (h == NULL)
-        return (0);
-    
-    while (h)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
-    
-    return (count);
+	size_t count = 0;
+
+	if (h == NULL)
+	return (0);
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	count++;
+	h = h->next;
+	}
+
+	return (count);
 }
