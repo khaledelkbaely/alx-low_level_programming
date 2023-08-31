bac3327a59150909bd88e2ec973648cc9b5fe820
khaled <carnageindia2003@gmail.com>
2023-08-31 22:05:07 +0300

    betty mistake

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -35,22 +35,22 @@ unsigned int _pow(unsigned int n, unsigned int power)
 
 unsigned int binary_to_uint(const char *b)
 {
-        int len = 0, i = 0;
-        unsigned int ret = 0;
+	int len = 0, i = 0;
+	unsigned int ret = 0;
 
-        if (b == NULL)
-                return (ret);
-        while (b[len])
-        {
-                if (b[len] != '0' && b[len] != '1')
-                        return ret;
-                len++;
-        }
+	if (b == NULL)
+		return (ret);
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (ret);
+		len++;
+	}
 
-        while (i < len)
-        {
-                ret += (b[i] - '0') * _pow(2, len - 1 - i);
-                i++;
-        }
-        return (ret);
+	while (i < len)
+	{
+		ret += (b[i] - '0') * _pow(2, len - 1 - i);
+		i++;
+	}
+	return (ret);
 }
