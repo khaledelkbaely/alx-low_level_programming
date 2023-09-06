aae2d5f179ca80e2c1a1b55169a4996d71dc634d
Empeshy <empeshy20036969@gmial.com>
2023-09-06 19:55:12 +0300

    file io #3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,23 +14,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int len;
+	int i = 0, file;
 
 	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
-	while (text_content[len])
-		len++;
 
-	if (write(fd, text_content, len) < 0)
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
 		return (-1);
-	close(fd);
+
+	write(file, text_content, i);
 
 	return (1);
 }
