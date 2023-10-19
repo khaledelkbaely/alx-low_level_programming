10f5e44601253257bba0c3b2d748f7620ab2046d
Empeshy <empeshy20036969@gmial.com>
2023-10-19 16:10:17 +0300

    del

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,7 +0,0 @@
-#ifndef OBJECT
-#define OBJECT
-#include "0-object_like_macro.h"
-
-#define SIZE 1024
-
-#endif /* OBJECT */
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUM_H
-#define SUM_H
-
-#include "4-sum.h"
-#define SUM(x, y) ((x) + (y))
-
-#endif /* SUM_H */
