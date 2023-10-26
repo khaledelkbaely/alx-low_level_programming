a24dc0e64c2967fa66109b958d91a0e8fca565cf
Empeshy <empeshy20036969@gmial.com>
2023-10-26 17:21:49 +0300

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt - entry point
+ *
+ * @n: number to find it sqrt
+ * @start: starting point of searching
+ *
+ * Return: the sqrt of n
+ */
+
+int _sqrt(int n, int start)
+{
+	if (start * start == n)
+		return (start);
+	else if (start * start > n)
+		return (-1);
+	return (_sqrt(n, start + 1));
+}
+
+/**
+ * _sqrt_recursion - entry point
+ *
+ * @n: number to sqrt
+ *
+ * Return: the sqrt of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
