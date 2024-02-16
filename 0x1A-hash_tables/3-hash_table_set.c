7021dba80c4cab306f096567d0941e13d9977e36
khaled <empeshy20036969@gmail.com>
2024-02-16 18:34:44 +0200

    5th

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,28 +13,70 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index;
 	hash_node_t *ptr, *new;
+	char *nvalue, *nkey;
 
-	if (!ht || !key || !(*key))
+	if (!ht || !ht->array || !ht->size || !key ||
+			!(*key) || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 
-	new = malloc(sizeof(hash_node_t *));
+	while (ptr)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			nvalue = strdup(value);
+			if (!nvalue)
+				return (0);
+			free(ptr->value);
+			ptr->value = nvalue;
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	new = make_hash_node(key, value);
 	if (!new)
 		return (0);
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
-	if (ptr)
-	{
-		while (ptr->next)
-		{
-			ptr = ptr->next;
-		}
-		ptr->next = new;
-	}
+	new->next = ht->array[index];
 	ht->array[index] = new;
 
 	return (1);
+}
+
+/**
+ * make_hash_node - make new hash node
+ *
+ * @key: is to index
+ * @value: value of the key
+ *
+ * Return: new node or NULL
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *nkey, *nvalue;
+
+	new = malloc(sizeof(hash_node_t *));
+	if (!new)
+		return (NULL);
+	nkey = strdup(key);
+	if (!nkey)
+	{
+		free(new);
+		return (NULL);
+	}
+	nvalue = strdup(value);
+	if (!nvalue)
+	{
+		free(new);
+		free(nkey);
+		return (NULL);
+	}
+
+	new->key = nkey;
+	new->value = nvalue;
+
+
+	return (new);
 }
